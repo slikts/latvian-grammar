22a8fe20a5d2a6e1f2e9ab4aefe6fd671c9ab353
slikts <dabas@untu.ms>
2020-07-14 15:46:43 +0000

    chore: fix exports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-// export { default as decline, Declension } from "./decline";
-// export { default as inflect } from "./inflect";
-// export { default as palatalize } from "./palatalize";
-// export * from "./util";
+export { default as decline, Declension } from "./decline";
+export { default as inflect } from "./inflect";
+export { default as palatalize } from "./palatalize";
+export * from "./util";
 
 // const ex1 = [
 //   "asins",
